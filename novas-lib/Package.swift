0979451983706455922cac2ec236ecd30982eecf
Jeff Doar <jdoar@swbell.net>
2024-12-15 13:06:37 -0600

    appear to be building a package successfully

--- novas-lib/Package.swift
+++ novas-lib/Package.swift
@@ -5,20 +5,26 @@ import PackageDescription
 
 let package = Package(
     name: "novas-lib",
+    platforms: [
+        SupportedPlatform.iOS(.v18),
+        SupportedPlatform.macOS(.v15),
+        SupportedPlatform.macCatalyst(.v18),
+        SupportedPlatform.visionOS(.v1),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "novas-lib",
-            targets: ["novas-lib"]),
+            targets: ["novas-c"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "novas-lib"),
-        .testTarget(
-            name: "novas-libTests",
-            dependencies: ["novas-lib"]
-        ),
-    ]
+            name: "novas-c",
+            dependencies: [],
+            path: "Sources/novas-c",
+            publicHeadersPath: "include")
+    ],
+    cLanguageStandard: CLanguageStandard.c89
 )
