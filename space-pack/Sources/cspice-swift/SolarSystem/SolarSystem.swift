713981d40b0caef228607676cafa5cde1b0c03cb
Jeff Doar <jdoar@swbell.net>
2024-12-26 17:42:38 -0600

    use barycenter IDs for Mars and outer planets

--- space-pack/Sources/cspice-swift/SolarSystem/SolarSystem.swift
+++ space-pack/Sources/cspice-swift/SolarSystem/SolarSystem.swift
@@ -26,11 +26,11 @@ public class SolarSystem {
             199: "Mercury" ,
             299: "Venus" ,
             399: "Earth",
-            499: "Mars" ,
-            599: "Jupiter" ,
-            699: "Saturn" ,
-            799: "Uranus" ,
-            899: "Neptune"
+            4: "Mars" ,
+            5: "Jupiter" ,
+            6: "Saturn" ,
+            7: "Uranus" ,
+            8: "Neptune"
         ]
         
         var localPlanets = [String: PlanetBody]()
